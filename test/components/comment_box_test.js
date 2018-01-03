a584e7446320fc324feb40d1a579e06af8fda89a
johnlyden <john.lyden86@gmail.com>
2018-01-02 20:55:12 -0600

    added assertion about textARea existing

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,7 +3,8 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
   it('has a text area', () => {
-
+    const component = renderComponent(CommentBox); // jQuery wrapped version of the component
+    expect(component.find('textarea')).to.exist;
   });
 
   it('it has a button', () => {
